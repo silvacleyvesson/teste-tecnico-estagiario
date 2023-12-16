abed1fcfa6a1905d65eaf98b009baaaeacd7f0a7
Cleyvesson Silva <37658986+silvacleyvesson@users.noreply.github.com>
2023-12-15 21:07:59 -0300

    Update page.jsx
    Resolução da Questão 1

--- src/app/questoes/1/page.jsx
+++ src/app/questoes/1/page.jsx
@@ -1,12 +1,25 @@
+const button =  {
+	display: 'center',
+	padding: '10px 20px',
+	fontSize: '16px',
+	cursor: 'pointer',
+	textAlign: 'center',
+	outline: 'none',
+	color: '#ffffff',
+	backgroundColor: '#ff0000',
+	border: 'none',
+	borderRadius: '16px',
+	boxShadow: '0 3px #999',
+   }  
 export default function Questao1() {
-  return (
-    <>
-      <h1>Questão 1</h1>
-      <p>
-        Crie um botão vermelho com as bordas arredondadas que exiba "Clique-me!"
-        como texto. Ao clicar no botão, um alerta deve ser disparado avisando
-        que o botão foi clicado.
-      </p>
-    </>
-  );
+  const botaoClicado=()=> {
+		alert("Botao Clicado!");
+	 }
+	return (
+		<>
+			<div>
+		    <button style={button} onClick={botaoClicado}>Clique Aqui!</button>
+			</div>
+		</>
+	);
 }
